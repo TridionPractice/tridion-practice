7bd3a86ef8c44658cc384143f08a2553d865b33c
dominic.cronin@gmail.com <dominic.cronin@gmail.com@9e8a06f9-e39e-197d-9016-2c31ad00f17b>
2011-07-14 05:45:38 +0000

    Added reference assembly
    git-svn-id: http://tridion-practice.googlecode.com/svn/trunk@4 9e8a06f9-e39e-197d-9016-2c31ad00f17b

---  tridion-practice --username dominic.cronin@gmail.com/PartitionComponentPresentations.cs
+++  tridion-practice --username dominic.cronin@gmail.com/PartitionComponentPresentations.cs
@@ -11,87 +11,107 @@ using Tridion.ContentManager;
 
 namespace ClassifyComponentPresentations
 {
-    abstract class PartitionComponentPresentations : ITemplate
-    {
-        Engine _engine;
-        Package _package;
-        virtual public void Transform(Engine engine, Package package)
-        {
-            _engine = engine;
-            _package = package;
-        }
+	abstract class PartitionComponentPresentations : ITemplate
+	{
+		Engine _engine;
+		Package _package;
+		virtual public void Transform(Engine engine, Package package)
+		{
+			_engine = engine;
+			_package = package;
+		}
 
-        protected void partitionCPs(IList<PartitionDescriptor> partitionDescriptors)
-        {
-            var pageItem = _package.GetByType(ContentType.Page);
-            if (pageItem == null)
-            {
-                // TODO - clean exception
-                throw new Exception("This TBB can only be used in a page template.");
-            }
-            var pageAsSource = pageItem.GetAsSource();
-            TcmUri pageUri = new TcmUri(pageAsSource.GetValue("ID"));
-            Page page = (Page)_engine.GetSession().GetObject(pageUri);
+		protected void partitionCPs(IList<PartitionDescriptor> partitionDescriptors)
+		{
+			var pageItem = _package.GetByType(ContentType.Page);
+			if (pageItem == null)
+			{
+				throw new WrongKindOfRenderException();
+			}
+			var pageAsSource = pageItem.GetAsSource();
+			TcmUri pageUri = new TcmUri(pageAsSource.GetValue("ID"));
+			Page page = (Page)_engine.GetSession().GetObject(pageUri);
 
-            var cpLists = new Dictionary<string, ComponentPresentationList>();
-            foreach (var pd in partitionDescriptors)
-            {
-                cpLists.Add(pd.name, new ComponentPresentationList()); 
-            }
+			var cpLists = new Dictionary<string, ComponentPresentationList>();
+			foreach (var pd in partitionDescriptors)
+			{
+				cpLists.Add(pd.name, new ComponentPresentationList());
+			}
 
-            try
-            {
-                cpLists.Add("Components", new ComponentPresentationList());
-            }
-            catch (Exception ex)
-            {
-                // TODO selectively catch and specifically throw
-                throw new Exception("Can't use Components, we use that - pick your own name.", ex);
-            }
-
-
-            foreach (var cp in page.ComponentPresentations)
-            {
-                Tridion.ContentManager.Templating.ComponentPresentation templatingCP =
-                    new Tridion.ContentManager.Templating.ComponentPresentation(
-                    new TcmUri(cp.Component.Id), new TcmUri(cp.ComponentTemplate.Id));
-
-                bool matched = false;
-                foreach(var pd in partitionDescriptors)
-                {
-                    if (pd.test(cp.Component, cp.ComponentTemplate ))
-                    {
-                        cpLists[pd.name].Add(templatingCP);
-                        matched = true;
-                        break;
-                    }
-                }
-                if (!matched)
-                {
-                    cpLists["Components"].Add(templatingCP );
-                }
-            }
+			try
+			{
+				cpLists.Add(Package.ComponentsName, new ComponentPresentationList());
+			}
+			catch (Exception ex)
+			{
+				// TODO selectively catch 
+                throw new ComponentsNameIsReservedException("Your own component presentation lists may not be called Components.", ex);
+			}
 
 
-        }
+			foreach (var cp in page.ComponentPresentations)
+			{
+				Tridion.ContentManager.Templating.ComponentPresentation templatingCP =
+					new Tridion.ContentManager.Templating.ComponentPresentation(
+					new TcmUri(cp.Component.Id), new TcmUri(cp.ComponentTemplate.Id));
 
-        protected struct PartitionDescriptor
-        {
-            public PartitionDescriptor(string name, Func<
-                    Tridion.ContentManager.ContentManagement.Component
-                    , Tridion.ContentManager.CommunicationManagement.ComponentTemplate
-                    , bool> test) 
-            
-            {
-                this.name = name;
-                this.test = test;
-            }
-            public string name;
-            public Func<
-                    Tridion.ContentManager.ContentManagement.Component 
-                    ,Tridion.ContentManager.CommunicationManagement.ComponentTemplate
-                    , bool> test;
-        }
+				bool matched = false;
+				foreach (var pd in partitionDescriptors)
+				{
+					if (pd.test(cp.Component, cp.ComponentTemplate))
+					{
+						cpLists[pd.name].Add(templatingCP);
+						matched = true;
+						break;
+					}
+				}
+				if (!matched)
+				{
+					cpLists[Package.ComponentsName].Add(templatingCP);
+				}
+			}
 
-    }
+
+		}
+
+		protected struct PartitionDescriptor
+		{
+			public PartitionDescriptor(string name, Func<
+					Tridion.ContentManager.ContentManagement.Component
+					, Tridion.ContentManager.CommunicationManagement.ComponentTemplate
+					, bool> test)
+			{
+				this.name = name;
+				this.test = test;
+			}
+			public string name;
+			public Func<
+					Tridion.ContentManager.ContentManagement.Component
+					, Tridion.ContentManager.CommunicationManagement.ComponentTemplate
+					, bool> test;
+		}
+
+		[Serializable]
+		public class ComponentsNameIsReservedException : Exception
+		{
+			public ComponentsNameIsReservedException() { }
+			public ComponentsNameIsReservedException(string message) : base(message) { }
+			public ComponentsNameIsReservedException(string message, Exception inner) : base(message, inner) { }
+			protected ComponentsNameIsReservedException(
+			  System.Runtime.Serialization.SerializationInfo info,
+			  System.Runtime.Serialization.StreamingContext context)
+				: base(info, context) { }
+		}
+
+		[Serializable]
+		public class WrongKindOfRenderException : Exception
+		{
+			public WrongKindOfRenderException() : base("This TBB is only useful in page templates.") { } 
+			public WrongKindOfRenderException( string message ) : base( message ) { }
+			public WrongKindOfRenderException( string message, Exception inner ) : base( message, inner ) { }
+			protected WrongKindOfRenderException( 
+			System.Runtime.Serialization.SerializationInfo info, 
+			System.Runtime.Serialization.StreamingContext context ) : base( info, context ) { }
+		}
+	}
 }
