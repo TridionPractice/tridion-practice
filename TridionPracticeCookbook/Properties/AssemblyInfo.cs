1ec4f9c5343ff158a2fd3b38f8f6c6fb4cd95185
dominic.cronin@gmail.com <dominic.cronin@gmail.com@9e8a06f9-e39e-197d-9016-2c31ad00f17b>
2011-07-23 11:13:01 +0000

    Reorganised everything into a cleaner structure. Tidied up the tests, renamed lots of things.
    git-svn-id: http://tridion-practice.googlecode.com/svn/trunk@13 9e8a06f9-e39e-197d-9016-2c31ad00f17b

--- TridionPracticeCookbook/Properties/AssemblyInfo.cs
+++ TridionPracticeCookbook/Properties/AssemblyInfo.cs
@@ -3,7 +3,8 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 using Tridion.ContentManager.Templating.Assembly;
-[assembly: TcmTemplateTitle("Assy TridionPractice")]
+[assembly: TcmTemplateTitle("TridionPractice.Cookbook")]
+[assembly: TcmTargetFolder("/webdav/Cookbook/Building%20Blocks/Templating")]
 
 [assembly: ComVisible(false)]
 
@@ -11,3 +12,5 @@ using Tridion.ContentManager.Templating.Assembly;
 [assembly: Guid("4f9bbd90-042d-4b76-8ca6-1c7c2ae70946")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyCopyright("Dominic Cronin 2011") ]
