fc1fed2cd59576d051c39a3e4711a7e190fa5ae8
Dominic Cronin <dominic.cronin@gmail.com>
2013-08-10 20:13:47 +0200

    Deleting old scripts that don't reflect current good practice.

--- TridionPowershellScripts/Get-TridionSession.ps1
+++
@@ -1,9 +0,0 @@
-Init-Tridion
-
-$session = new-object Tridion.ContentManager.Session
-$getNewObjectMI = [Tridion.ContentManager.Session].GetMethod("GetNewObject")
-add-member -InputObject $session -MemberType ScriptMethod -Name "GetNewUser" -Value {
-  [Tridion.ContentManager.Session].GetMethod(
-  "GetNewObject").MakeGenericMethod([Tridion.ContentManager.Security.User]).Invoke($this, @())
-}
-return $session
