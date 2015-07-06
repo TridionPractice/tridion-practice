97bc4624c1c6ac0563cebb1cb329493d0ac68664
Peter Kjaer <pkjaer.sdl@gmail.com>
2015-07-06 13:45:52 +0200

    Claims are now sorted by the claim URI on the client-side.

--- javascripts/AmbientDataFramework/ClaimsViewModel.js
+++ javascripts/AmbientDataFramework/ClaimsViewModel.js
@@ -27,6 +27,12 @@ function ClaimsViewModel() {
 				result.push(new Claim(key, data[key]));
 			}
 			
+			result.sort(function(a,b) {
+				if (a.key > b.key) { return 1; }
+				if (a.key < b.key) { return -1;}
+				return 0;
+            });
+						
 			self.allClaims(result);
 			self.loading(false);
 		});
